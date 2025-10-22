d9b9e4803a0cc1797d9188e4b53e52cad4680fb8
CaeNavarrete-Dev <caetanonavarrete@gmail.com>
2025-10-22 16:42:46 -0300

    Add files via upload

---
+++ VLDocesWeb/Repositories/CustomerMemoryRepository.cs
@@ -0,0 +1,27 @@
+using VLDocesWeb.Models;
+
+namespace VLDocesWeb.Repositories;
+
+public class CustomerMemoryRepository : ICustomerRepository
+{
+    public static List<Customer> _customers = new List<Customer>();
+    public Customer Login(LoginViewModel model)
+    {
+        foreach (Customer c in _customers)
+        {
+            if(c.Email == model.Email && c.Senha == model.Senha)
+            {
+                return c;
+            }
+        }
+                
+        return null;
+    }
+
+    public void Register(Customer customer)
+    {
+        customer.Id = _customers.Count + 1;
+        _customers.Add(customer);
+        return;
+    }
+}
