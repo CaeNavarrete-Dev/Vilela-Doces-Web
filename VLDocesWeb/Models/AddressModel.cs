7a00a0896ed6c4a0009deb2eff1f463eeebbe791
Marina Sabino <marisabinocarvalho@gmail.com>
2025-10-27 19:15:47 -0300

    Criação da Model de Endereço

---
+++ VLDocesWeb/Models/AddressModel.cs
@@ -0,0 +1,11 @@
+namespace VLDocesWeb.Models;
+
+public class Address {
+  public int AddressId { get; set; }
+  public string Rua { get; set; }
+  public string Numero { get; set; }
+  public string Bairro { get; set; }
+  public string Cep { get; set; }
+  public string Complemento { get; set; }
+  public int ClienteId { get; set; }
+}
