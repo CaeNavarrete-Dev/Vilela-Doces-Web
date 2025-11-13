eb9a801a8ceaf182ee6d00a92fe363a11ac93554
CaeNavarrete-Dev <caetanonavarrete@gmail.com>
2025-11-12 23:59:24 -0300

    Tentando fazer o Detalhes do Pedido

--- VLDocesWeb/obj/Debug/net9.0/VLDocesWeb.AssemblyInfo.cs
+++ VLDocesWeb/obj/Debug/net9.0/VLDocesWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VLDocesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1911aa103bf2ad48a757627fdd7f9a6a056bc7bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2089342c48002a4ba00f0c9edeeb1752ee501fb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("VLDocesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VLDocesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
